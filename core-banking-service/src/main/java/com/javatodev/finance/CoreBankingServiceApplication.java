467f54bc5e813b0b1547a9ecbaecffa7580726a6
LeonardoRSantos <leonardoworkbench@gmail.com>
2021-09-12 11:36:52 -0300

    BankingService

---
+++ core-banking-service/src/main/java/com/javatodev/finance/CoreBankingServiceApplication.java
@@ -0,0 +1,18 @@
+package com.javatodev.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CoreBankingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreBankingServiceApplication.class, args);
+
+	}
+
+}
---
+++ core-banking-service/src/main/java/com/javatodev/finance/model/TransactionType.java
@@ -0,0 +1,5 @@
+package com.javatodev.finance.model;
+
+public enum TransactionType {
+    FUND_TRANSFER, UTILITY_PAYMENT
+}
